34f24c6bb016675706bdb770c4cbd593d2c923e3
João Victor Gregório da Silva <146145021+joaovictorgregorio@users.noreply.github.com>
2024-03-12 14:59:08 -0300

    Projeto Simon Game finalizado.

--- class projects/Simon Game Challenge Starting Files/game.js
+++ class projects/Simon Game Challenge Starting Files/game.js
@@ -1,80 +1,71 @@
-var buttonColours = [ "red", "blue", "green", "yellow" ];
+var buttonColours = [ "red", "blue", "green", "yellow" ],
+    gamePattern = [],
+    userClickedPattern = [],
+    started = false,
+    level = 0;
 
-var gamePattern = [];
+// Detecta o pressionamento de alguma tecla para iniciar o game
+$( document ).keydown( function () {
+    // Forma de descobrir se o jogo foi iniciado ou não
+    if ( !started ) {
+        $( "#level-title" ).text( "Nível " + level );
+        nextSequence();
+        started = true;
+    }
+} );
 
-var userClickedPattern = [];
+// Função utilizando jQuery para pegar o click do usuário
+$( ".btn" ).click( function() {
+    var userChosenColour = $( this ).attr( "id" );
+    
+    userClickedPattern.push( userChosenColour );
 
-var gameStarted = false;
+    playSound( userChosenColour );
+    animatePress( userChosenColour );
+    checkAnswer( userClickedPattern.length - 1 );
+} );
 
-var level = 0;
+// Função do jogo execução da sua lógica
+function checkAnswer( currentLevel ) {
+    if ( gamePattern[ currentLevel ] === userClickedPattern[ currentLevel ] ) {
+        if ( userClickedPattern.length === gamePattern.length ) {
+            setTimeout( function () {
+                nextSequence();
+            }, 1000 );
+        }
+    } else {
+        playSound("wrong");
+        $( "body" ).addClass( "game-over" );
+        $( "#level-title" ).text( "Game Over, pressione qualquer tecla para reiniciar" );
+        
+        setTimeout( function() {
+            $( "body" ).removeClass( "game-over" );
+        }, 200 );
 
+        startOver();
+    }
+}
+
+
+// Função inicial com os parâmetros de lógica para processamento
 function nextSequence() {
+    // Aumento de nível toda vez que a função next é chamada
+    userClickedPattern = [];
+    level++;
+    $( "#level-title" ).text( "Nível " + level );
+
     var randomNumber = Math.round( Math.random() * 3 );
 
     var randomChosenColour = buttonColours[ randomNumber ];
 
     gamePattern.push( randomChosenColour );
 
-    var selectedButton = $( "#" + randomChosenColour );
+    $( "#" + randomChosenColour ).fadeIn( 100 ).fadeOut( 100 ).fadeIn( 100 );
 
-    selectedButton.fadeOut(150).fadeIn(150);
-
-    switch ( randomChosenColour ) {
-        case "red":
-            var soundRed = new Audio("./sounds/red.mp3");
-            soundRed.play();
-            break;
-
-        case "blue":
-            var soundBlue = new Audio("./sounds/blue.mp3");
-            soundBlue.play();
-            break;
-
-        case "green":
-            var soundGreen = new Audio("./sounds/green.mp3");
-            soundGreen.play();
-            break;
-
-        case "yellow":
-            var soundYellow = new Audio("./sounds/yellow.mp3");
-            soundYellow.play();
-            break;
-
-        default: alert("Recarregue a página!")
-    }
-
-    level++;
-    updateLevelTitle();
-} 
-
-function playSound( name ) {
-
-    switch ( name ) {
-        case "red":
-            var soundRed = new Audio("./sounds/red.mp3");
-            soundRed.play();
-            break;
-
-        case "blue":
-            var soundBlue = new Audio("./sounds/blue.mp3");
-            soundBlue.play();
-            break;
-
-        case "green":
-            var soundGreen = new Audio("./sounds/green.mp3");
-            soundGreen.play();
-            break;
-
-        case "yellow":
-            var soundYellow = new Audio("./sounds/yellow.mp3");
-            soundYellow.play();
-            break;
-
-        default: alert("Recarregue a página!")
-    }
-    
+    playSound( randomChosenColour );
 }
 
+// Função para colocar a animação nas cores, quando pressionado ou selecionado pelo sistema do jogo, com a técnica de adicionar e remover a classe depois de certo tempo personalizado!
 function animatePress( currentColour ) {
     $( "#" + currentColour ).addClass( "pressed" );
 
@@ -83,28 +74,16 @@ function animatePress( currentColour ) {
     }, 100 );
 }
 
-function manipulating() {
-    var userChosenColour = $( this ).attr( "id" );
+// Função para realizar o som do botão
+function playSound( name ) {
 
-    userClickedPattern.push( userChosenColour );
-    
-    playSound( userChosenColour );
-
-    animatePress( userChosenColour );
+    var audio = new Audio( "./sounds/" + name + ".mp3" );
+    audio.play();
 }
 
-function handleKeydown( event ) {
-    if ( !gameStarted ) {
-        gameStarted = true;
-        nextSequence();
-        updateLevelTitle();
-    }
+// Função para reiniciar o jogo novamente, após o erro do usuário
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
 }
-
-function updateLevelTitle() {
-    $( "#level-title" ).text( "Nível " + level );
-}
-
-$( document ).keydown( handleKeydown );
-$( ".btn" ).click( nextSequence );
-
