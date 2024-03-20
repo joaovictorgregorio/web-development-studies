220551161905aedf3a72f39912ed4238a8344c58
João Victor Gregório da Silva <146145021+joaovictorgregorio@users.noreply.github.com>
2024-03-20 10:56:52 -0300

    Update index.js

--- studies in class/seção 23 - Node.js/2.4 QR Code Project/index.js
+++ studies in class/seção 23 - Node.js/2.4 QR Code Project/index.js
@@ -4,5 +4,13 @@
 3. Create a txt file to save the user input using the native fs node module.
 */
 
-const fs = require("fs");
+const fs = require( "fs" );
 
+fs.readFile( "./mensagem-usuario.txt", "utf8", ( err, data ) => {
+    if ( err ) throw err;
+    console.log( data );
+  });
+
+  import inquirer from "inquirer";
+
+  var url_usuario = prompt();
