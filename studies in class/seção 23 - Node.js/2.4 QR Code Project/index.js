b3db6e00b65654869d4b3b4060476d4b37987b5e
João Victor Gregório da Silva <146145021+joaovictorgregorio@users.noreply.github.com>
2024-03-28 20:38:00 -0300

    Projeto qr code image.

--- studies in class/seção 23 - Node.js/2.4 QR Code Project/index.js
+++ studies in class/seção 23 - Node.js/2.4 QR Code Project/index.js
@@ -1,16 +1,23 @@
-/* 
-3. Create a txt file to save the user input using the native fs node module.
-*/
-
 import inquirer from "inquirer";
 import qr from "qr-image";
+import fs from "fs";
 
 inquirer
   .prompt([
-    /* Pass your questions in here */
+    {
+      message: "Type in your URL: ",
+      name: "URL",
+    },
   ])
   .then((answers) => {
-    // Use user feedback for... whatever!!
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr-image.png"));
+
+    fs.writeFile('URL.txt', url, (err) => {
+      if (err) throw err;
+      console.log('The file has been saved!');
+    }); 
   })
   .catch((error) => {
     if (error.isTtyError) {
