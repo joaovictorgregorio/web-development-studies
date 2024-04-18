7f29bb64141ea0e44a8c8dc0272ffb51b9125e6a
Joao Victor Gregorio <joaogregoriodsilva@gmail.com>
2024-04-18 12:46:29 -0300

    Avançando nos estudos Express.js, utilizando package Nodemon e método Get no servidor

--- studies in class/Backend/seção 24 - Express.js with Node.js/3.2 HTTP Requests/index.js
+++ studies in class/Backend/seção 24 - Express.js with Node.js/3.2 HTTP Requests/index.js
@@ -3,7 +3,15 @@ const app = express();
 const port = 6500;
 
 app.get("/", (req, res) => {
-    res.send("Olá, bem-vindo...");
+    res.send("<strong>Página principal, funcionamento OK</strong>");
+});
+
+app.get("/contato", (req, res) => {
+    res.send("<strong><ul>Contato</ul></strong><li>99 99999999</li>");
+});
+
+app.get("/sobre", (req, res) => {
+    res.send("<strong><h2>Sobre</h2></strong><li>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</li>");
 });
 
 app.listen(port, () => {
