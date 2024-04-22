18f31ec52cacb082119a838400aeb58d1d9c8869
Joao Victor Gregorio <joaogregoriodsilva@gmail.com>
2024-04-22 11:26:49 -0300

    Desafio concluído sobre middlewares, gerador de nomes de bandas

--- studies in class/Backend/seção 24 - Express.js with Node.js/3.4 Middleware/index1.js
+++ studies in class/Backend/seção 24 - Express.js with Node.js/3.4 Middleware/index1.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
 import bodyParser from "body-parser";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
--- studies in class/Backend/seção 24 - Express.js with Node.js/3.4 Middleware/index4.js
+++ studies in class/Backend/seção 24 - Express.js with Node.js/3.4 Middleware/index4.js
@@ -1,8 +1,34 @@
+/* Desafio do módulo 3.4 Middleware */
+
 import express from "express";
+import { dirname }  from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
 
-const app = express();
-const port = 3000;
+var aplicacao = express();
+var porta = 4500;
+var diretorio = dirname(fileURLToPath(import.meta.url));
+var nomeDaBanda = "";
 
-app.listen(port, () => {
-  console.log(`Servidor em funcionamento na porta ${port}`);
+// Analisa o aplicativo
+aplicacao.use(bodyParser.urlencoded({ extended : true }));
+
+function geradorDeNomesDasBandas (req, res, next) {
+  console.log(req.body);
+  nomeDaBanda = req.body["street"] + " " + req.body["pet"];
+  next();
+}
+
+aplicacao.use(geradorDeNomesDasBandas);
+
+aplicacao.get("/", (req, res) => {
+  res.sendFile(diretorio + "/public/home_page.html");
+});
+
+aplicacao.post("/submit", function(req, res) {
+  res.send(`<h1>Sua banda se chama: </h1><h3>${nomeDaBanda} 🫶🏾</h3>`);
+});
+
+aplicacao.listen(porta, () => {
+  console.log(`Servidor em funcionamento na porta ${porta}`);
 });
