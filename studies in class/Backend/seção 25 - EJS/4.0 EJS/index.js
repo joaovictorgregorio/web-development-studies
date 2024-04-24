dbc12e1aec447676f39b1b30300d27bb44bc432b
Joao Victor Gregorio <joaogregoriodsilva@gmail.com>
2024-04-24 11:15:42 -0300

    Inicio dos estudos sobre EJS, desafio realizado aplicando seus conceitos.

---
+++ studies in class/Backend/seção 25 - EJS/4.0 EJS/index.js
@@ -0,0 +1,28 @@
+import expres from "express";
+
+var app = expres();
+var port = 3000;
+
+app.get("/", function (req, res) {
+    var dataAtual = new Date();
+    var diaSemanaNumero = dataAtual.getDay();
+
+    console.log(diaSemanaNumero);
+    var mensagemSemana = "é um dia da semana, é hora de trabalhar pesado!"
+    var menssagemFimDeSemana = "é fim de semana, é hora de descansar e pegar mais leve!";
+
+    if (diaSemanaNumero === 0 || diaSemanaNumero === 6) {
+        res.render("index.ejs", {
+            mensagem: menssagemFimDeSemana,
+        });
+    }
+    else {
+        res.render("index.ejs", {
+            mensagem: mensagemSemana,
+        });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Em funcionamento na porta ${port}`);
+});
